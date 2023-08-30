c46a11555799cf1219780b5c9c36c50e3a508e1e
Rasmus-Kibshede <2740ralle@gmail.com>
2023-08-30 12:11:31 +0200

    Strukture done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home"
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
