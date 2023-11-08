e828174ae6af2c4f1c3b979d7b4ad1996f1b063b
Rasmus Kibshede <2740ralle@gmail.com>
2023-11-08 09:42:06 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainMenuNavBar from './components/ui/MainMenuNavBar';
 import Home from './pages/Home/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/Signup';
+import Layout from './components/ui/Layout';
 
 function App() {
   return (
     <>
       <Router>
-        <MainMenuNavBar />
         <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path='/' element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
         </Routes>
       </Router>
     </>
