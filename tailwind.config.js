a20390938847f60bc5351caea903188002175447
Rasmus Kibshede <2740ralle@gmail.com>
2023-11-20 23:16:58 +0100

    added: tailwind for main styling ability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: '#root',
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        logo: "url('src/assets/images/logo.png')",
+      },
+      colors: {
+        primary: '#ff0000',
+        secondary: '#00ff00',
+        tertiary: '#0000ff',
+      },
+    },
+    fontFamily: {
+      main: 'Josefin Sans, sans-serif',
+    },
   },
   plugins: [],
-}
+};
