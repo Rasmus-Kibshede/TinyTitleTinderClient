e60cc801d4de534f85034872575114a36593019e
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2023-11-21 19:50:18 +0100

    init commit

--- src/paths/urls.ts
+++ src/paths/urls.ts
@@ -4,3 +4,5 @@ export const checkauthPath = `${base}checkauth`;
 
 // TODO: Make dynamic so that ID is not hardcoded but dependent on the signed in user
 export const getName = `${base}names/parent/1`;
+
+export const getNames = `${base}names`;
