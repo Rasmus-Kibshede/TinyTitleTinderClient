4581248d6a7011f1ac74c765c35c20946c222775
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2023-11-24 10:31:12 +0100

    Side bar for dev

---
+++ src/ui/components/SideNavBar.tsx
@@ -0,0 +1,71 @@
+import {
+    Drawer,
+    Box,
+    Typography,
+    IconButton,
+    List,
+    ListItemText,
+    ListItemIcon,
+    ListItemButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { AccountBox } from "@mui/icons-material/";
+import GradeIcon from "@mui/icons-material/Grade";
+import LinkIcon from '@mui/icons-material/Link';
+
+//TODO: Add more menu items
+const menuItems = [
+    {
+        text: "Account",
+        icon: <AccountBox color="primary" />,
+        path: "accountSettings",
+    },
+    {
+        text: "Liked names",
+        icon: <GradeIcon color="primary" />,
+        path: "likedNames",
+    },
+    {
+        text: "Connected caregivers",
+        icon: <LinkIcon color="primary" />,
+        path: "connectedCaregivers",
+    },
+];
+
+export const SideDrawer = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <IconButton
+                size="large"
+                edge="start"
+                color="default"
+                aria-label="menu"
+                onClick={() => setOpen(true)}
+                sx={{ ml: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+                <Box width="250px" textAlign="center">
+                    <Typography variant="h6" component="div">
+                        Settings
+                    </Typography>
+                    <List>
+                        {menuItems.map((item) => (
+                            <Link to={item.path} key={item.text}>
+                                <ListItemButton>
+                                    <ListItemIcon>{item.icon}</ListItemIcon>
+                                    <ListItemText primary={item.text} />
+                                </ListItemButton>
+                            </Link>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+        </>
+    );
+};
