74d18ff845beadd3a47dce85c7f2ff234d2612e4
Joachim Tyrfing Richter <73465595+Fladbrod@users.noreply.github.com>
2023-12-13 21:42:04 +0100

    Update src/paths/urls.ts
    Co-authored-by: Rasmus Kibshede <2740ralle@gmail.com>

--- src/paths/urls.ts
+++ src/paths/urls.ts
@@ -5,7 +5,9 @@ const base = 'http://localhost:3000/';
 
 export const getNames = `${base}names`;
 export const getName = `${base}names/parent/`;
-export const updateTableNames = `${base}parents/${id}/tables`;
+export const updateTableNames = (id: number) => {
+  return `${base}parents/${id}/tables`;
+};
 export const checkAuthPath = `${base}checkauth`;
 export const logout = `${base}logout`;
 export const login = `${base}login`;
